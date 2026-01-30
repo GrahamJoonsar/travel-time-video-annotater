dff68748a1019355aae684c5fc4506a412cd5bd0
Joonsar <gtj162@icloud.com>
2026-01-30 13:57:25 -0500

    Finished JSON functionality

--- src/marker.py
+++ src/marker.py
@@ -3,8 +3,6 @@
 Marker: This class represents a point in a video at a particular time and screen coordinates
 Marker Manager: This class keeps track of all the markers for a particular instance of video,
                 and is responsible for saving, reading, and displaying markers, among other functions.
-
-TODO: Change the marker saving and reading to work with json instead.
 """
 
 # Used for reading and writing the marker files
@@ -12,7 +10,6 @@ import json
 from datetime import datetime
 
 class Marker:
-    HIGHEST_ID = 0 # Static class variable neccesary to keep IDs unique between markers
     def __init__(self, 
                  pos: tuple[int, int], 
                  frame_num: int,
@@ -23,12 +20,8 @@ class Marker:
         self.pos = pos
         self.frame_num = frame_num
         
-        # When setting a new ID, do one larger than the previous highest
-        if id is None:
-            self.id = Marker.HIGHEST_ID + 1
-            Marker.HIGHEST_ID = self.id
-        else:
-            self.id = id
+        # Marker manager will maintain uniqueness between IDs
+        self.id = id
 
         # The id of the marker that this one is paired with
         self.paired_id = paired_id
@@ -46,14 +39,54 @@ class Marker:
             "paired_id": self.paired_id,
         }
 
+
+# Class that collects marker instances for a specific video,
+# ensuring that IDs are unique and paired IDs are managed properly
 class Marker_Manager:
+    HIGHEST_ID = 0 # Static class variable neccesary to keep IDs unique between markers
     def __init__(self):
         self.markers = {}
+        self.frame_step = None
+
+    def add_marker(self, pos: tuple[int, int], frame_num: int):
+        Marker_Manager.HIGHEST_ID += 1
+        self.markers[Marker_Manager.HIGHEST_ID] = Marker(
+            pos,
+            frame_num,
+            Marker_Manager.HIGHEST_ID,
+            None
+        )
     
-    # Your input should be a properly formatted JSON file with marker data
-    def read_markers(self, marker_path: str):
-        # TODO
-        pass
+    # Your input should be a properly formatted JSON file with marker data,
+    # function returns the frame_step and the video path in (int, str)
+    # Function will error on FileNotFound, should be handled by user
+    def read_markers(self, marker_path: str) -> tuple[int, str]:
+        with open(marker_path) as f:
+            # Erasing marker data possibly previously contained, and resetting HIGHEST ID
+            self.markers = {}
+            Marker_Manager.HIGHEST_ID = 0
+            marker_data = json.load(f)
+            # Retrieve header data then remove, leaving only marker data
+            ret = (marker_data["frame_step"], marker_data["vid_path"])
+            keys_to_remove = []
+            for key in marker_data:
+                if not key.isdigit():
+                    keys_to_remove.append(key)
+            for key in keys_to_remove:
+                del marker_data[key]
+            
+            # Turn each marker entry into a Marker object
+            for key in marker_data:
+                self.markers[int(key)] = Marker(
+                    pos=(marker_data[key]["x"], marker_data[key]["y"]),
+                    frame_num=marker_data[key]["frame_num"],
+                    id=int(key),
+                    paired_id=marker_data[key]["paired_id"]
+                )
+                if Marker_Manager.HIGHEST_ID < int(key):
+                    Marker_Manager.HIGHEST_ID = int(key)
+
+            return ret
 
 
     # Writes the marker dataframe to the specified JSON path
@@ -61,6 +94,8 @@ class Marker_Manager:
     def write_markers(self, marker_path: str, frame_step: int, vid_path: str):
         with open(marker_path, "w") as f:
             output_dict = {}
+
+            # Adding header data
             output_dict["frame_step"] = frame_step
             output_dict["vid_path"] = vid_path
             formatted_datetime = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
